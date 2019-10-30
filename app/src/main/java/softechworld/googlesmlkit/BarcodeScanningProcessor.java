87bc4b3511a39fa0b880c7a64b239b3d4e145734
priyanshu patel <patel.priyanshu@hotmail.com>
2019-10-30 19:34:13 +0530

    Add to git

---
+++ app/src/main/java/softechworld/googlesmlkit/BarcodeScanningProcessor.java
@@ -0,0 +1,70 @@
+package softechworld.googlesmlkit;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
+import java.io.IOException;
+import java.util.List;
+
+/** Barcode Detector Demo. */
+public class BarcodeScanningProcessor extends VisionProcessorBase<List<FirebaseVisionBarcode>> {
+
+    private static final String TAG = "BarcodeScanProc";
+
+    private final FirebaseVisionBarcodeDetector detector;
+
+    public BarcodeScanningProcessor() {
+        // Note that if you know which format of barcode your app is dealing with, detection will be
+        // faster to specify the supported barcode formats one by one, e.g.
+        // new FirebaseVisionBarcodeDetectorOptions.Builder()
+        //     .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE)
+        //     .build();
+        detector = FirebaseVision.getInstance().getVisionBarcodeDetector();
+    }
+
+    @Override
+    public void stop() {
+        try {
+            detector.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Exception thrown while trying to close Barcode Detector: " + e);
+        }
+    }
+
+    @Override
+    protected Task<List<FirebaseVisionBarcode>> detectInImage(FirebaseVisionImage image) {
+        return detector.detectInImage(image);
+    }
+
+    @Override
+    protected void onSuccess(
+            @NonNull List<FirebaseVisionBarcode> barcodes,
+            @NonNull FrameMetadata frameMetadata,
+            @NonNull GraphicOverlay graphicOverlay) {
+        graphicOverlay.clear();
+        for (int i = 0; i < barcodes.size(); ++i) {
+            FirebaseVisionBarcode barcode = barcodes.get(i);
+            BarcodeGraphic barcodeGraphic = new BarcodeGraphic(graphicOverlay, barcode);
+            graphicOverlay.add(barcodeGraphic);
+
+            makeToast(barcode);
+        }
+    }
+
+    private void makeToast(FirebaseVisionBarcode barcode) {
+        public static Toast.makeText(this,barcode.toString(),Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onFailure(@NonNull Exception e) {
+        Log.e(TAG, "Barcode detection failed " + e);
+    }
+}
